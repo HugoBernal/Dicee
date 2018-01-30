bbaf5ba78c19c660ff6b9856ba63fdf85773d857
Hugo Bernal <hugonobernal@gmail.com>
2018-01-29 19:21:23 -0500

    Implemented the function motionEnded for updating the dices images whenever the phone stops being shaken.

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -47,5 +47,11 @@ class ViewController: UIViewController {
         
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages()
+        
+    }
+    
 }
 
